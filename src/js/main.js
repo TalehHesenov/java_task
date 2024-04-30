82318f09f07cc7761fb699d65783fb8bdabf5d48
TalehHesenov <127445846+TalehHesenov@users.noreply.github.com>
2024-04-30 09:48:58 +0300

    task edit

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
 var text = prompt("Catdirilma adresin yoxlayin");
 var address = "Baki";
-
 if(text=="address"){
 
     alert("Cattirilma ugurla tamamlandi");
-
+}
+else if(text == ""){
+    alert("Bos buraxmaq olmaz");
 }
 else{
     alert("Adrese catrdirilma yoxdur");
 }
-
 
